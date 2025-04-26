a1a0f5999ebb622ce8f6f859458643d42e06e8c9
norton <3078788052.@qq.com>
2025-04-26 22:26:13 +0800

    板块添加

---
+++ spike_service/src/main/java/org/spike_service/SpikeServiceApplication.java
@@ -0,0 +1,13 @@
+package org.spike_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpikeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpikeServiceApplication.class, args);
+    }
+
+}
